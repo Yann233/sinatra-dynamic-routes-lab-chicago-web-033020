c5a0e305f15a5ad7000cd6cf9fd74d4c2e1e7afe
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-21 01:04:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   # Create a dynamic route at get '/square/:number' that accepts a number and returns the square of that number.
   # Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
   get '/square/:number' do
-    params[:number].to_i ** 2
+    number = params[:number].to_i ** 2
   end
 
 
