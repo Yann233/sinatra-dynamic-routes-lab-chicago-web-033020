b54c3ec506a59920ed2cc3a11e0ee8f51ca2d12b
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-21 01:03:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ class App < Sinatra::Base
     params[:name].reverse
   end
 
+  # Create a dynamic route at get '/square/:number' that accepts a number and returns the square of that number. 
+  # Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
+  get '/square/:number' do
+    params[:number].to_i ** 2
+  end
 
 
-  # Create a dynamic route at get '/square/:number' that accepts a number and returns the square of that number. Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
+  
   #
   # Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a single string the number of times given.
   #
